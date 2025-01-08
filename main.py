be57a15f27ba06a1fa7afbcaf0e44c023094743b
neto test <129604391+xeweles@users.noreply.github.com>
2025-01-08 22:47:25 +0500

    change main

--- main.py
+++ main.py
@@ -6,36 +6,39 @@ def acounting(file: str) -> int:
 
 
 # 1 Задача
-with open('recipes.txt', encoding='UTF-8') as file:
-    cook_book = {}
-    for i in file:
-        recepie_name = i.strip()
-        ingredients_count = file.readline()
-        ingredients = []
-        for p in range(int(ingredients_count)):
-            recepie = file.readline().strip().split(' | ')
-            product, quantity, word = recepie
-            ingredients.append({'product': product, 'quantity': quantity, 'measure': word})
-        file.readline()
-        cook_book[recepie_name] = ingredients
+def create_dict():
+    with open('recipes.txt', encoding='UTF-8') as file:
+        cook_book = {}
+        for i in file:
+            recepie_name = i.strip()
+            ingredients_count = file.readline()
+            ingredients = []
+            for p in range(int(ingredients_count)):
+                recepie = file.readline().strip().split(' | ')
+                product, quantity, word = recepie
+                ingredients.append({'product': product, 'quantity': quantity, 'measure': word})
+            file.readline()
+            cook_book[recepie_name] = ingredients
+    return cook_book
 
 # 2 Задача
 def get_shop_list_by_dishes(person_count: int, dishes: list):
+    cook_book = create_dict()
     result = {}
     for dish in dishes:
         if dish in cook_book:
             for consist in cook_book[dish]:
                 if consist['product'] in result:
-                    result[consist['product']]['quantity'] += consist['quantity'] * person_count
+                    result[consist['product']]['quantity'] += int(consist['quantity']) * person_count
                 else:
                     result[consist['product']] = {'measure': consist['measure'],
-                                                  'quantity': (consist['quantity'] * person_count)}
+                                                  'quantity': (int(consist['quantity']) * person_count)}
         else:
             print('Такого блюда нет в книге')
     print(result)
 
 
-get_shop_list_by_dishes(2, ['Запеченный картофель', 'Омлет'])
+get_shop_list_by_dishes(2, ['Омлет', 'Омлет'])
 
 
 # 3 Задача
